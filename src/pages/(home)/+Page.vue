fe49a19c62afd966f99c03750c460e921561d007
Filip Sufitchi <fsufitchi@gmail.com>
2025-11-06 00:58:29 +0000

    Add a sample button in vue as well

--- src/pages/(home)/+Page.vue
+++ src/pages/(home)/+Page.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import { NA } from 'naive-ui';
+import { NA, NButton } from 'naive-ui';
 </script>
 
 <template>
   <h1>Home Page</h1>
   <p>Welcome to the home page!</p>
+  <p>This is a button rendered in Vue: <NButton>Click me!</NButton></p>
   <NA href="mdxtest"> Go to mdxtest</NA>
 </template>
