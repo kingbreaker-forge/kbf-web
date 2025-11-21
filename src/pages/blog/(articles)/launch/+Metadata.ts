764cadb4f07c2d9ffa58530c1e6da44b3ef3b3fe
Filip Sufitchi <fsufitchi@gmail.com>
2025-11-21 01:57:53 +0000

    Create a proper 'first post'

--- src/pages/blog/(articles)/launch/+Metadata.ts
+++ src/pages/blog/(articles)/launch/+Metadata.ts
@@ -2,10 +2,11 @@ import type { BlogArticle } from '@kb-web/useBlog';
 
 export default {
   blog: {
-    title: 'Website Launch!',
+    title: 'Website Launch',
     authors: ['Filip Sufitchi', 'Kingbreaker Forge'],
-    date: new Date('2024-06-15'),
-    summary: 'Announcing the launch of our new platform!',
+    date: new Date('2025-11-20'),
     // coverImageUrl: 'https://example.com/images/launch-cover.jpg',
+    summary: `Announcing the launch of the website.
+    Probably not the in-depth content you're looking for, but hey. It's here.`,
   } satisfies BlogArticle,
 };
